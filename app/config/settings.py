e20efb6c87070fc9aa2957493db9a06e9a33f8a8
B Sreenath Reddy <45588260+sreenath361@users.noreply.github.com>
2026-02-13 12:59:47 +0530

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,12 +4,13 @@ import os
 load_dotenv()
 
 class Settings:
-    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    # GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 
     ALLOWED_MODEL_NAMES =[
-        "llama3-70b-8192",
-        "llama-3.3-70b-versatile"
+        "gpt-4o-mini",
+        "gpt-4o"
     ]
 
 settings=Settings()
